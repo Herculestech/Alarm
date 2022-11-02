451f531217d17d863ace1463b532eb507c36160f
Herculestech <ajalaisaacpromise18@gmail.com>
2022-11-02 15:25:08 -0500

    i added local date and time

--- alarm.js
+++ alarm.js
@@ -1,44 +1,47 @@
-const newTime = () => {
-    const savedTime = (new Date().toLocaleTimeString())
-    normalTime.innerHTML = `${savedTime}`
-    normalTime.style.color = "green"
-    normalTime.style.fontWeight = "900"
-    normalTime.style.fontSize = "70px"
+const alarmSong = new Audio("Juice_WRLD_ft_Halsey_-_Life_s_A_Mess_360media.com.ng.mp3")
+alarmSong.loop=true
+let alarmTime = null
+let alarmTimeout = null
+
+const updateTime = () => {
+    const date = new Date()
+
+    const hour = timeFormat(date.getHours())
+    const minutes = timeFormat(date.getMinutes())
+    const seconds = timeFormat(date.getSeconds())
+    disp.innerHTML = `${hour} : ${minutes} : ${seconds}`
+    disp.style.color = "green"
+    disp.style.fontWeight = "900"
+    disp.style.fontSize = "77px"
 }
-setInterval(newTime, 1000);
 
-
-
-
-let myAlarmSong = new Audio("Juice_WRLD_ft_Halsey_-_Life_s_A_Mess_360media.com.ng.mp3")
-let wrongAnswer = new Audio("Juice_WRLD_-_Rich_and_Blind.mp3")
-
-const setAlarm = () =>{
-    let myHours = hour.value;
-    let myMinutes = min.value;
-    let Mysec=sec.value;
-    n = new Date();
-    if(myHours == n.getHours() && myMinutes == n.getMinutes() && Mysec == n.getSeconds()){
-        min.value=""
-        hour.value=""
-        sec.value=""
-        myAlarmSong.play()
+function timeFormat(time) {
+    if (time < 10){
+        return '0' + time
     }
-    setTimeout(setAlarm, 1000);
+    return time
+}
+function setAlarmTime(value) {
+    alarmTime = value
 }
 
-const stopAlarm = () =>{
-    alert('Wanna stop this solve this equation')
-    let stp = prompt("What is 2x + 2 =6,find x");
-    if (stp == 2){
-        myAlarmSong.pause()
-        // wrongAnswer.play()
-        alert('Wake up')
-        location.reload() 
-    }else{
-        alert("Can't stop this without solving the eqaution")
-        myAlarmSong.pause()
-        wrongAnswer.play()
-        notCorrectAnswer.loop
+const setAlarm = () => {
+        if(alarmTime) {
+    const current = new Date()
+    const timeToAlarm = new Date(alarmTime)
+
+    if (timeToAlarm > current) {
+        const timeout = timeToAlarm.getTime() - current.getTime()
+        alarmTimeout = setTimeout(() => alarmSong.play(), timeout)
+        alert('Alarm set')
+    }
+  }
+}
+function clearAlarm() {
+    alarmSong.pause()
+    if (alarmTimeout) {
+        clearTimeout(alarmTimeout)
+        alert('Alarm cleared')
     }
 }
+setInterval(updateTime, 1000);
